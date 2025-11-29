bff4835e483f2dcba49110a3e106aa197978a56b
Andrey Antiporovich <andrey.antiporovich@protonmail.com>
2025-11-29 12:11:27 +0100

    kan-51 modify class fortress

--- fortresses.py
+++ fortresses.py
@@ -1,15 +1,19 @@
 import turtle as t
 
+type numeric = int | float
+
 class Fortress(t.Turtle):
-    def __init__(self, x: int | float, y: int | float):
+    def __init__(self, x: numeric, y: numeric):
         super().__init__()
         self.penup()
         self.color("green")
         self.teleport(x, y)
         self.shape("square")
         self.lifes = 10
+        self.radius = 5
         
     def hit(self) -> None:
         self.lifes -= 1
-        if self.lifes <= 0:
-            self.hideturtle()
+        
+    def destroy(self) -> None:
+        self.hideturtle() 
