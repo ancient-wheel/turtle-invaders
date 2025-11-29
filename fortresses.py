8bac26a539807484e0e3127357b5f54874f22c79
Andrey Antiporovich <andrey.antiporovich@protonmail.com>
2025-11-29 14:27:54 +0100

    change order of rotaging colors

--- fortresses.py
+++ fortresses.py
@@ -1,6 +1,9 @@
 import turtle as t
+from itertools import cycle
 
 type numeric = int | float
+
+COLORS = cycle(("blue", "white", "yellow", "red", "green"))
 
 class Fortress(t.Turtle):
     def __init__(self, x: numeric, y: numeric):
@@ -10,10 +13,11 @@ class Fortress(t.Turtle):
         self.teleport(x, y)
         self.shape("square")
         self.lifes = 10
-        self.radius = 5
+        self.radius = 10
         
     def hit(self) -> None:
         self.lifes -= 1
+        self.color(next(COLORS))
         
     def destroy(self) -> None:
         self.hideturtle() 
