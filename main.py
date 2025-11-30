10275f047ef429f01e664422cec573879d13d188
Andrey Antiporovich <andrey.antiporovich@protonmail.com>
2025-11-30 13:13:32 +0100

    kan-63 refactoring

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ class App():
         self.bullets = []
         self.to_remove = ItemsToRemove()
         self.tasks = Queue()
-        self.tasks_gui = Queue()
+        self.tasks_main = Queue()
         self.run = True
         self.fortresses = None
         self.cooldown_user_shoot = 0.5
@@ -116,7 +116,7 @@ class App():
         if len(self.bullets) > 0:
             logger.debug("Reset list of bullets")
             [
-                self.tasks_gui.put(bullet.destroy) for bullet in self.bullets
+                self.tasks_main.put(bullet.destroy) for bullet in self.bullets
             ]
             rlock.acquire()
             self.to_remove.bullets.update([i for i in range(len(self.bullets))])
@@ -194,8 +194,8 @@ class App():
             ):
                 logger.debug("Bullet hit user (%s, %s)", bullet.xcor(), bullet.ycor())
                 self.to_remove.bullets.add(i)
-                self.tasks_gui.put(self.game_lifes.reduce_)
-                self.tasks_gui.put(self.game_lifes.update)
+                self.tasks_main.put(self.game_lifes.reduce_)
+                self.tasks_main.put(self.game_lifes.update)
                 self.tasks.put(self.reset_bullets)
                 self.tasks.put(self.check_lifes)
             for fortress in self.fortresses:
@@ -204,8 +204,8 @@ class App():
                 ):
                     logger.debug("Bullet hit fortress (%s, %s)", bullet.xcor(), bullet.ycor())
                     self.tasks.put(fortress.hit)
-                    self.tasks_gui.put(fortress.change_color)
-                    self.tasks_gui.put(bullet.destroy)
+                    self.tasks_main.put(fortress.change_color)
+                    self.tasks_main.put(bullet.destroy)
                     self.to_remove.bullets.add(i)
             for col, rows in enumerate(self.invaders):
                 for row, invader in enumerate(rows):
@@ -215,17 +215,17 @@ class App():
                         invader.heading() != bullet.heading()
                     ):
                         logger.debug("Bullet hit invader (%s, %s)", bullet.xcor(), bullet.ycor())
-                        self.tasks_gui.put(invader.destroy)
-                        self.tasks_gui.put(bullet.destroy)
+                        self.tasks_main.put(invader.destroy)
+                        self.tasks_main.put(bullet.destroy)
                         self.to_remove.bullets.add(i)
                         self.tasks.put((lambda: self.game_score.increase(1)))
-                        self.tasks_gui.put(self.game_score.update)
+                        self.tasks_main.put(self.game_score.update)
                         self.to_remove.invaders.add((col, row))
                     
     def check_lifes(self,) -> None:
         if self.game_lifes.value <= 0:
             logger.info("User lost all life points")
-            self.tasks_gui.put(self.game_over)
+            self.tasks_main.put(self.game_over)
             self.stop()
             
     def check_invaders_win(self) -> bool:
@@ -248,8 +248,8 @@ class App():
             logger.debug("Updating level")
             self.tasks.put(self.reset_bullets)
             self.tasks.put((lambda: self.game_level.increase(1)))
-            self.tasks_gui.put(self.initialize_invaders)
-            self.tasks_gui.put(self.game_level.update)
+            self.tasks_main.put(self.initialize_invaders)
+            self.tasks_main.put(self.game_level.update)
             self.tasks.put(self.increase_game_speed)
             logger.debug("Updated level")
             
@@ -307,7 +307,7 @@ def main():
         if app.check_invaders_win():
             app.stop()
             app.game_over()
-        perform_tasks(app.tasks_gui)
+        perform_tasks(app.tasks_main)
         app.screen.update()
         sleep(0.001)
     logger.info("Main thread is stopping...")
