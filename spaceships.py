84964d1b240e1b606630a547c031ce3b546ded0e
Andrey Antiporovich <andrey.antiporovich@protonmail.com>
2025-12-02 15:15:20 +0100

    refactoring: replace lossy constants by IntEnum

--- spaceships.py
+++ spaceships.py
@@ -1,44 +1,47 @@
 from __future__ import annotations
 import turtle as t
+from enum import IntEnum
 
 type numeric = int | float
+
+class ObjectDirection(IntEnum):
+    NORTH = 90
+    SOUTH = 270
 
 class SpaceShip(t.Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.penup()
         self.shape("turtle")
-        self.left(90)
+        self.left(ObjectDirection.NORTH.value)
         self.color("green")
         self.teleport(0, -330)
         self.radius = 10
         
     def shoot(self) -> Bullet:
-        return Bullet(self.xcor(), self.ycor(), N, "green")
+        return Bullet(self.xcor(), self.ycor(), ObjectDirection.NORTH, "green")
     
 class Invader(t.Turtle):
     def __init__(self, x: numeric, y: numeric) -> None:
         super().__init__()
         self.penup()
         self.shape("turtle")
-        self.right(90)
+        self.left(ObjectDirection.SOUTH.value)
         self.color("blue")
         self.teleport(x, y)
         self.radius = 10
     
     def shoot(self) -> Bullet:
-        return Bullet(self.xcor(), self.ycor(), S, self.color()[0])
+        return Bullet(self.xcor(), self.ycor(), ObjectDirection.SOUTH, self.color()[0])
 
     def destroy(self) -> None:
         self.hideturtle()
         
-N = 90
-S = 270
 class Bullet(t.Turtle):
-    def __init__(self, x: numeric, y: numeric, direction: int, color: str) -> None:
+    def __init__(self, x: numeric, y: numeric, direction: ObjectDirection, color: str) -> None:
         super().__init__()
         self.penup()
-        self.left(direction)
+        self.left(direction.value)
         self.color(color)
         self.teleport(x, y)
         self.radius = 3
