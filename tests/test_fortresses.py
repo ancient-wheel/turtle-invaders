512f32137142ff263f356d51c5b77e8823b7764a
Andrey Antiporovich <andrey.antiporovich@protonmail.com>
2025-12-03 09:04:06 +0100

    refactoring pytest fortresses

--- tests/test_fortresses.py
+++ tests/test_fortresses.py
@@ -1,37 +1,43 @@
 from turtle_invaders.fortresses import Fortress
-from pytest import fixture
-from pytest import raises
+import pytest
 
-def test_fortress_float():
-    dut = Fortress(0.0, 0.0)
-    assert isinstance(dut, Fortress)
+VALID_PARAMETERS = (
+    (0, 0),
+    (0.0, 0.0),
+    (0.0, 0),
+    (0, 0.0),
+)
 
-def test_fortress_int():
-    dut = Fortress(0, 0)
-    assert isinstance(dut, Fortress)
-    
-def test_fortress_str_1():
-    with raises(TypeError):
-        dut = Fortress("0", 0)
+INVALID_PARAMETERS = (
+    (0, "0",),
+    ("0", 0),
+)
 
-def test_fortress_str_2():
-    with raises(TypeError):
-        dut = Fortress(0, "0")
+type numeric = int | float
 
-@fixture
-def test_fortress_fixture():
+@pytest.mark.parametrize("x, y", VALID_PARAMETERS)
+def test_parametrized_valid_fortness(x: numeric, y: numeric) -> None:
+    Fortress(x, y)
+
+@pytest.mark.parametrize("x, y", INVALID_PARAMETERS)
+def test_fortress_str_2(x: numeric, y: numeric) -> None:
+    with pytest.raises(TypeError):
+        dut = Fortress(x, y)
+
+@pytest.fixture
+def fortress_fixture() -> Fortress:
     return Fortress(0, 0)
     
-def test_hit(test_fortress_fixture):
-    old_lifes = test_fortress_fixture.lifes
-    test_fortress_fixture.hit()
-    new_lifes = test_fortress_fixture.lifes
+def test_hit(fortress_fixture: Fortress) -> None:
+    old_lifes = fortress_fixture.lifes
+    fortress_fixture.hit()
+    new_lifes = fortress_fixture.lifes
     assert old_lifes - new_lifes == 1
     
-def test_change_color(test_fortress_fixture):
-    test_fortress_fixture.change_color()
-    assert test_fortress_fixture.color()[0] == "blue"
+def test_change_color(fortress_fixture: Fortress) -> None:
+    fortress_fixture.change_color()
+    assert fortress_fixture.color()[0] == "blue"
 
-def test_destroy(test_fortress_fixture):
-    test_fortress_fixture.destroy()
-    assert test_fortress_fixture.isvisible() == False
+def test_destroy(fortress_fixture: Fortress) -> None:
+    fortress_fixture.destroy()
+    assert fortress_fixture.isvisible() == False
